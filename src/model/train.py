e80664c565c3055bbce31b528705876b76b57bae
j4nb00n <j4nb00n@gmail.com>
2023-08-10 10:42:10 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -3,16 +3,16 @@
 import argparse
 import glob
 import os
-
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+import mlflow
 
 
 # define functions
 def main(args):
-    # TO DO: enable autologging
-
+    # enable autologging
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -33,7 +33,14 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    mlflow.autolog()
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
+               'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
+               'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
@@ -46,8 +53,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
+    parser.add_argument("--training_data",
+                        dest='training_data', type=str)
     parser.add_argument("--reg_rate", dest='reg_rate',
                         type=float, default=0.01)
 
@@ -56,6 +63,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
